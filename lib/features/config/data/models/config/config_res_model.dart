c8b3fa457a21306313d869bcf4ce0affafd2ad59
d9y0 <boycotthub.app@gmail.com>
2023-12-21 23:37:34 +0300

    add config features and add config json for github pages

---
+++ lib/features/config/data/models/config/config_res_model.dart
@@ -0,0 +1,20 @@
+import 'package:boycott_hub/features/config/data/models/config/nested/apps_config_model.dart';
+import 'package:boycott_hub/features/config/data/models/config/nested/chngelog_model.dart';
+import 'package:boycott_hub/features/config/data/models/config/nested/update_info_model.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'config_res_model.freezed.dart';
+part 'config_res_model.g.dart';
+
+@freezed
+class ConfigResModel with _$ConfigResModel {
+  factory ConfigResModel({
+    @JsonKey(name: 'configs_urls') required List<String> configs,
+    @JsonKey(name: 'update_info') required UpdateInfoModel updateInfo,
+    required ChngelogModel chngelog,
+    @JsonKey(name: 'main_dynamic_widget') required Map<String, dynamic> mainDynamicWidget,
+    @JsonKey(name: 'apps_config') required AppsConfigModel appsConfig,
+  }) = _ConfigResModel;
+
+  factory ConfigResModel.fromJson(Map<String, dynamic> json) => _$ConfigResModelFromJson(json);
+}
